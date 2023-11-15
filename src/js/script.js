d56d9623d3b2248d0cad27a9eaef810328287720
ma-mani <mani.mashayekhi3@gmail.com>
2023-11-15 22:07:54 +0100

    finish darkmode switcher

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+// Select all Ellements
+
+const showAnswer = document.querySelector('[data-js="button"]');
+const showAnswer1 = document.querySelector('[data-js="button-1"]');
+
+const textAnswer = document.querySelector("[data-js='card__answer']");
+const textAnswer1 = document.querySelector("[data-js='card__answer-1']");
+
+const selectBookmark1 = document.querySelector("[data-js='bookmark-1']");
+const selectBookmark2 = document.querySelector("[data-js='bookmark-2']");
+
+// Events
+
+showAnswer.addEventListener('click', () => {
+    if (showAnswer.textContent === 'Show Answer') {
+        showAnswer.textContent = 'Hide Answer';
+        textAnswer.classList.remove('card__hide-answer');
+    } else {
+        textAnswer.classList.add('card__hide-answer');
+        showAnswer.textContent = 'Show Answer';
+    }
+});
+
+showAnswer1.addEventListener('click', () => {
+    if (showAnswer1.textContent === 'Show Answer') {
+        showAnswer1.textContent = 'Hide Answer';
+        textAnswer1.classList.remove('card__hide-answer');
+    } else {
+        textAnswer1.classList.add('card__hide-answer');
+        showAnswer1.textContent = 'Show Answer';
+    }
+});
+
+selectBookmark1.addEventListener('click', () => {
+    selectBookmark1.classList.toggle('--green');
+});
+
+selectBookmark2.addEventListener('click', () => {
+    selectBookmark2.classList.toggle('--green');
+});
+
+console.log(window.scrollY);
